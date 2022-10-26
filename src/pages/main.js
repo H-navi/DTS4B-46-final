37e619935528f305473048ec75d9928172d518da
H-navi <hanafinaufal@protonmail.com>
2022-10-26 22:14:44 +0700

    update readme

--- src/pages/main.js
+++ src/pages/main.js
@@ -2,9 +2,8 @@ import logo from '../logo.svg';
 // import './App.css';
 // import { Outlet } from 'react-router-dom';
 
-import HeaderRecomendation from '../containers/HeaderRecomendation';
-import Header from '../containers/Header';
 import Sliders from '../containers/Sliders';
+import Header from '../containers/Header';
 // import Trending from './containers/Trending';
 // import Featured from './containers/Featured';
 // import VideoPosts from './containers/VideoPosts';
